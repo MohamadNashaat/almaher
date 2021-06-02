29c1974281add36957e7c87dbfd2c3fffadeb4a4
Mohamad Nashaat <mohamad.nashaat99@gmail.com>
2021-06-02 17:02:40 +0300

    Fix index page

--- home/views.py
+++ home/views.py
@@ -70,27 +70,30 @@ def index(request):
     c_student = Person.objects.all().filter(type_id='Student').count()
     c_graduate = Person.objects.all().filter(type_id='Graduate').count()
     c_course = Course.objects.all().count()
-    c_session = Session.objects.filter(course_id=get_course_id).count()
+    session = Session.objects.filter(course_id=get_course_id)
+    c_session = session.count()
+    c_session_student = Session_Student.objects.filter(session_id__in=session).count()
 
-    level = Level.objects.all()
-    student_count = []
-    teacher_count = []
-    level_list = level.values_list('level_name', flat=True)
-    for level_loop in level_list:
-        s_count = Person.objects.filter(type_id='Student' ,level_id=level_loop).count()
-        t_count = Person.objects.filter(type_id__in=('Teacher', 'Graduate') ,level_id=level_loop, status=True).count()
-        student_count.append(s_count)
-        teacher_count.append(t_count)
-    zip_list = zip(level_list, student_count)
-    zip_list2 = zip(level_list, teacher_count)
+    #level = Level.objects.all()
+    #student_count = []
+    #teacher_count = []
+    #level_list = level.values_list('level_name', flat=True)
+    #for level_loop in level_list:
+    #    s_count = Person.objects.filter(type_id='Student' ,level_id=level_loop).count()
+    #    t_count = Person.objects.filter(type_id__in=('Teacher', 'Graduate') ,level_id=level_loop, status=True).count()
+    #    student_count.append(s_count)
+    #    teacher_count.append(t_count)
+    #zip_list = zip(level_list, student_count)
+    #zip_list2 = zip(level_list, teacher_count)
 
     context = {'c_teacher': c_teacher,
                 'c_student': c_student,
                 'c_graduate': c_graduate,
                 'c_course': c_course,
                 'c_session': c_session,
-                'get_course_id': get_course_id,
-                'zip_list': zip_list,
-                'zip_list2': zip_list2,
+                'c_session_student': c_session_student,
+                'get_course_id': get_course_id,            
+                #'zip_list': zip_list,
+                #'zip_list2': zip_list2,
                 }
     return render(request, 'home/index.html', context)
